88f40a945c73b304966e28c6eacf5f87c4d5d6cf
Evan Salter <esalter@vendasta.com>
2016-09-09 20:51:08 -0600

    Update pivotal links when title changes

--- src/content_script.js
+++ src/content_script.js
@@ -1,6 +1,7 @@
-var PIVOTAL_REGEX = /\[#[0-9]+\]/;
+var PIVOTAL_REGEX = /\[#[0-9]+\]/g;
 var NON_NUMERIC_REPLACE_REGEX = /\D/g;
-var BETTER_TRELLO_PIVOTAL = 'better-trello-pivotal';
+var BETTER_TRELLO_PIVOTAL_LABELS = 'better-trello-pivotal-labels';
+var BETTER_TRELLO_PIVOTAL_TITLES = 'better-trello-pivotal-titles';
 
 var pivotal = function() {
     var elements = document.getElementsByClassName("list-card");
@@ -12,40 +13,42 @@ var pivotal = function() {
 }
 
 function checkTitlesForPivotal(el) {
-    clearPivotals(el);
+    clearPivotals(el, BETTER_TRELLO_PIVOTAL_TITLES);
     var titleEl = el.getElementsByClassName("list-card-title")[0];
-    checkForPivotal(el, titleEl);
+    checkForPivotal(el, titleEl, BETTER_TRELLO_PIVOTAL_TITLES);
 }
 
 function checkLabelsForPivotal(el) {
-    clearPivotals(el);
+    clearPivotals(el, BETTER_TRELLO_PIVOTAL_LABELS);
     var labelEl = el.getElementsByClassName('list-card-labels')[0];
     labels = labelEl.childNodes;
     for (var i = 0; i < labels.length; i++) {
-        checkForPivotal(el, labels[i]);
+        checkForPivotal(el, labels[i], BETTER_TRELLO_PIVOTAL_LABELS);
     }
 }
 
-function checkForPivotal(parentEl, el) {
+function checkForPivotal(parentEl, el, type) {
     var elText = el.innerText;
     var match = elText.match(PIVOTAL_REGEX);
     if (match) {
-        var pivotalNumber = match[0].replace(NON_NUMERIC_REPLACE_REGEX, '');
-        addPivotalLink(parentEl, pivotalNumber);
+        match.forEach(function(pivotal) {
+            var pivotalNumber = match[0].replace(NON_NUMERIC_REPLACE_REGEX, '');
+            addPivotalLink(parentEl, pivotalNumber, type);
+        });
     }
 }
 
-function clearPivotals(el) {
-    var currentPivotals = el.getElementsByClassName(BETTER_TRELLO_PIVOTAL);
+function clearPivotals(el, type) {
+    var currentPivotals = el.getElementsByClassName(type);
     while (currentPivotals.length > 0) {
         currentPivotals[0].remove();
     }
 }
 
-function addPivotalLink(el, pivotalNumber) {
+function addPivotalLink(el, pivotalNumber, type) {
     var badgesEl = el.getElementsByClassName('badges')[0];
     var pivotalEl = document.createElement('span')
-    pivotalEl.classList.add(BETTER_TRELLO_PIVOTAL);
+    pivotalEl.classList.add(type);
     pivotalEl.classList.add('badge-icon');
     pivotalEl.classList.add('icon-sm');
     var imageUrl = chrome.extension.getURL('assets/pivotal.png');
@@ -58,12 +61,22 @@ function addPivotalLink(el, pivotalNumber) {
 
 pivotal();
 
-var observer = new MutationObserver(function (mutations) {
+var labelObserver = new MutationObserver(function (mutations) {
     var parent = mutations[0].target.parentElement.parentElement;
     checkLabelsForPivotal(parent);
 });
 
-observer.observe(document.querySelector(".list-card-labels"), {
+labelObserver.observe(document.querySelector(".list-card-labels"), {
+    childList: true,
+    subtree:true
+});
+
+var titleObserver = new MutationObserver(function (mutations) {
+    var parent = mutations[0].target.parentElement.parentElement;
+    checkTitlesForPivotal(parent);
+});
+
+titleObserver.observe(document.querySelector(".list-card-title"), {
     childList: true,
     subtree:true
 });
