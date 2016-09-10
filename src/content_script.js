8fded2149eae30b397fad176f94ebaf4e4d0b7c0
Evan Salter <esalter@vendasta.com>
2016-09-09 20:23:29 -0600

    Automatically update pivotal links

--- src/content_script.js
+++ src/content_script.js
@@ -1,48 +1,51 @@
 var PIVOTAL_REGEX = /\[#[0-9]+\]/;
 var NON_NUMERIC_REPLACE_REGEX = /\D/g;
+var BETTER_TRELLO_PIVOTAL = 'better-trello-pivotal';
 
 var pivotal = function() {
     var elements = document.getElementsByClassName("list-card");
     for (var i = 0; i < elements.length; i++) {
         var el = elements[i];
-        el.addEventListener('change', function() {
-            checkTitlesForPivotal(el);
-            checkLabelsForPivotal(el);
-        });
         checkTitlesForPivotal(el);
         checkLabelsForPivotal(el);
     }
 }
 
 function checkTitlesForPivotal(el) {
+    clearPivotals(el);
     var titleEl = el.getElementsByClassName("list-card-title")[0];
-    var titleText = titleEl.innerText;
-    var match = titleText.match(PIVOTAL_REGEX);
-    if (match) {
-        var pivotalNumber = match[0].replace(NON_NUMERIC_REPLACE_REGEX, '');
-        titleText = titleText.replace(match[0], '')
-        titleEl.innerText = titleText;
-        addPivotalLink(el, pivotalNumber)
-    }
+    checkForPivotal(el, titleEl);
 }
 
 function checkLabelsForPivotal(el) {
+    clearPivotals(el);
     var labelEl = el.getElementsByClassName('list-card-labels')[0];
     labels = labelEl.childNodes;
     for (var i = 0; i < labels.length; i++) {
-        var labelEl = labels[i];
-        var labelText = labelEl.innerText;
-        var match = labelText.match(PIVOTAL_REGEX);
-        if (match) {
-            var pivotalNumber = match[0].replace(NON_NUMERIC_REPLACE_REGEX, '');
-            addPivotalLink(el, pivotalNumber);
-        }
+        checkForPivotal(el, labels[i]);
+    }
+}
+
+function checkForPivotal(parentEl, el) {
+    var elText = el.innerText;
+    var match = elText.match(PIVOTAL_REGEX);
+    if (match) {
+        var pivotalNumber = match[0].replace(NON_NUMERIC_REPLACE_REGEX, '');
+        addPivotalLink(parentEl, pivotalNumber);
+    }
+}
+
+function clearPivotals(el) {
+    var currentPivotals = el.getElementsByClassName(BETTER_TRELLO_PIVOTAL);
+    while (currentPivotals.length > 0) {
+        currentPivotals[0].remove();
     }
 }
 
 function addPivotalLink(el, pivotalNumber) {
     var badgesEl = el.getElementsByClassName('badges')[0];
     var pivotalEl = document.createElement('span')
+    pivotalEl.classList.add(BETTER_TRELLO_PIVOTAL);
     pivotalEl.classList.add('badge-icon');
     pivotalEl.classList.add('icon-sm');
     var imageUrl = chrome.extension.getURL('assets/pivotal.png');
@@ -54,3 +57,13 @@ function addPivotalLink(el, pivotalNumber) {
 }
 
 pivotal();
+
+var observer = new MutationObserver(function (mutations) {
+    var parent = mutations[0].target.parentElement.parentElement;
+    checkLabelsForPivotal(parent);
+});
+
+observer.observe(document.querySelector(".list-card-labels"), {
+    childList: true,
+    subtree:true
+});
